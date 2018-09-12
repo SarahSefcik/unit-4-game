44e1ef361fb7ff9bd4273c1b1bac620ca0e1bcf1
Sarah Catherine Sefcik <sefciksa@gmail.com>
2018-09-12 17:41:27 -0400

    Pseudocode the game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// Create Variables
+// Function to generate random number between 19-120
+// Reset Function for values of marshmallow images array. Values between 1-12.
+// For loop to generate new values
+// Reset Function to alter the HTML displayed on screen
+// Reset Function to completely reset everything on the page
+// Call the functions to run on the page for setup
+// Function when a marshmallow is clicked, what happens?
+// When the first image is clicked, a value is counted
+// The number displays as text (score-number) where the variable counter is to display
+// If statements
+// equal to goal, win and reset
+// greater than goal, loss and reset
+// Call function to run on the page when clicked
