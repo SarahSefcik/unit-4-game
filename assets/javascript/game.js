2edfec56a7fddf32b969e5d9b17b8116308b4dda
Sarah Catherine Sefcik <sefciksa@gmail.com>
2018-09-12 18:23:36 -0400

    added JS code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,41 @@
 // Create Variables
+
+var goal = "";
+var wins = 0;
+var losses = 0;
+var counter = 0;
+var images = ["https://via.placeholder.com/50x50", "https://via.placeholder.com/50x50", "https://via.placeholder.com/50x50", "https://via.placeholder.com/50x50"];
+
 // Function to generate random number between 19-120
+
+function randomTargetNumber() {
+  goal = Math.floor(Math.random() * 101) + 19;
+}
+
 // Reset Function for values of marshmallow images array. Values between 1-12.
-// For loop to generate new values
+
+function resetMarshmallows() {
+
+  // For loop to generate new values
+  for (var i = 0; i < images.length; i++) {
+    var marshmallows = $("<img>");
+    marshmallows.addClass("m_m");
+    marshmallows.attr("src", images[i]);
+    marshmallows.attr("value", (Math.floor(Math.random() * 13) + 1));
+    marshmallows.attr("height", "100");
+    $(".marshmallow_Images").append(marshmallows);
+  }
+}
+
 // Reset Function to alter the HTML displayed on screen
+
+function alterHTML() {
+  $(".goal").html("<h5>Goal Score: " + goal + "</h5>");
+  $(".win_Loss").html("<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>");
+  $(".score-number").html(counter);
+  $(".marshmallow_Images").empty();
+}
+
 // Reset Function to completely reset everything on the page
 // Call the functions to run on the page for setup
 // Function when a marshmallow is clicked, what happens?
