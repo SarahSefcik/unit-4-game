df8337972d6297d0871fb0c4cb387ca222b9fb3e
Sarah Catherine Sefcik <sefciksa@gmail.com>
2018-09-12 20:16:04 -0400

    possibly finished .js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
 // Create Variables
-
 var goal = "";
 var wins = 0;
 var losses = 0;
@@ -7,13 +6,11 @@ var counter = 0;
 var images = ["https://via.placeholder.com/50x50", "https://via.placeholder.com/50x50", "https://via.placeholder.com/50x50", "https://via.placeholder.com/50x50"];
 
 // Function to generate random number between 19-120
-
 function randomTargetNumber() {
   goal = Math.floor(Math.random() * 101) + 19;
 }
 
 // Reset Function for values of marshmallow images array. Values between 1-12.
-
 function resetMarshmallows() {
 
   // For loop to generate new values
@@ -28,7 +25,6 @@ function resetMarshmallows() {
 }
 
 // Reset Function to alter the HTML displayed on screen
-
 function alterHTML() {
   $(".goal").html("<h5>Goal Score: " + goal + "</h5>");
   $(".win_Loss").html("<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>");
@@ -37,11 +33,39 @@ function alterHTML() {
 }
 
 // Reset Function to completely reset everything on the page
+function Reset() {
+  randomTargetNumber();
+  counter = 0;
+  alterHTML();
+  resetMarshmallows();
+}
+
 // Call the functions to run on the page for setup
+randomTargetNumber();
+alterHTML();
+resetMarshmallows();
+
 // Function when a marshmallow is clicked, what happens?
-// When the first image is clicked, a value is counted
-// The number displays as text (score-number) where the variable counter is to display
-// If statements
-// equal to goal, win and reset
-// greater than goal, loss and reset
+function marshmallowClick() {
+
+  // When the first image is clicked, a value is counted
+  counter += parseInt($(this).attr("value"));
+
+  // The number displays as text (score-number) where the variable counter is to display
+  $(".score-number").html(counter);
+
+  // If statements
+  // equal to goal, win and reset
+  if (counter == goal) {
+    wins++;
+    Reset();
+  }
+  // greater than goal, loss and reset
+  else if (counter > goal) {
+    losses++;
+    Reset();
+  };
+};
+
 // Call function to run on the page when clicked
+$(document).on("click", ".m_m", marshmallowClick);
